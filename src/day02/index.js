18d3bf9f822ba97d89dda3f093429b746a715500
Paweł Biniak <pawix00@gmail.com>
2025-12-12 11:03:38 +0300

    day 2

--- src/day02/index.js
+++ src/day02/index.js
@@ -3,15 +3,58 @@ import run from "aocrunner";
 const parseInput = (rawInput) => rawInput;
 
 const part1 = (rawInput) => {
-  const input = parseInput(rawInput);
+  const input = parseInput(rawInput).replace(/\r/g, "")
+    .split("\n")[0]
+    .split(",")
+    .map(x => {
+      const parts = x.split("-");
+      const minimum = parseInt(parts[0]);
+      const maximum = parseInt(parts[1]);
+      return Array(maximum - minimum + 1).fill(0).map((_, i) => i + minimum);
+    })
+    .flat();
 
-  return;
+  let count = 0;
+  for (const num of input) {
+    const str = num.toString();
+    if (str.length % 2 === 0 && str.slice(0, str.length / 2) === str.slice(str.length / 2)) count += num;
+  }
+
+  console.log(count);
+
+  return count;
 };
 
 const part2 = (rawInput) => {
-  const input = parseInput(rawInput);
+  const input = parseInput(rawInput).replace(/\r/g, "")
+    .split("\n")[0]
+    .split(",")
+    .map(x => {
+      const parts = x.split("-");
+      const minimum = parseInt(parts[0]);
+      const maximum = parseInt(parts[1]);
+      return Array(maximum - minimum + 1).fill(0).map((_, i) => i + minimum);
+    })
+    .flat();
 
-  return;
+  let count = 0;
+  for (const num of input) {
+    const str = num.toString();
+    for (let i = 1; i <= str.length / 2; i++) {
+      if (str.length % i === 0) {
+        let j = i;
+        for (; j < str.length; j += i) {
+          if (str.slice(j - i, j) !== str.slice(j, j + i)) break;
+        }
+        if (j >= str.length) {
+          count += num;
+          break;
+        }
+      }
+    }
+  }
+
+  return count;
 };
 
 run({
